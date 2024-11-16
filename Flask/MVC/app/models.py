ced44be1421a78d88ef8f3bf8eefbb15386d4e2a
João Vieira <netojoao599@gmail.com>
2024-11-16 14:19:06 -0300

    Migrate and comit

--- Flask/MVC/app/models.py
+++ Flask/MVC/app/models.py
@@ -8,5 +8,6 @@ class Contact(db.Model):
     email = db.Column(db.String, nullable=True)
     subejct = db.Column(db.String, nullable=True)
     message = db.Column(db.String, nullable=True)
-    date_send = db.Column(db.DateTime, default=datetime.utnow()) # or datetime.now()
+    respon = db.Column(db.Integer, default=True)
+    date_send = db.Column(db.DateTime, default=datetime.utcnow()) # or datetime.now()
 
