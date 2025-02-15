f400c8f2086564e87ae046ae43965ead23ac30c8
João Vieira <netojoao599@gmail.com>
2025-02-14 23:06:49 -0300

    Repositórios e inserção de dados

--- Mongo/run.py
+++ Mongo/run.py
@@ -1,20 +1,35 @@
 from models.connection_options.connection import DBConnectionHandler
+from models.repository.minhaCollection_repository import MinhaCollectionRepository
 
 
 if __name__ == "__main__":
 
     db_handle = DBConnectionHandler()
-    conn1 = db_handle.get_db_connection()
-    print(conn1)
-
-    print("+-----------------------------------------------------------------------------------------------------------+")
-
     db_handle.connect_to_db()
-    conn2 = db_handle.get_db_connection()
-    print(conn2)
+    db_connection = db_handle.get_db_connection()
 
-    collection = conn2.get_collection("minhaCollection")
-    collection.insert_one({
-        "Estou": "Inserindo pelo run",
-        "Numeros": [987, 654, 321]
-    })
+    minha_collection_repository = MinhaCollectionRepository(db_connection)
+
+    
+    order = {
+        "nome": "Jaum",
+        "endereco": "Rua La",
+        "pedidos": {
+            "hotdog": 2,
+            "pizza": 2,
+            "esfiha": 5
+        },
+        "cpf": '132.dgt.337'
+    }
+
+    minha_collection_repository.insert_document(order)
+
+    list_of_documents = [
+        {"RBR": "Super Max"},
+        {"Aston Martin": "Strollada"},
+        {"Ferrari": "Halmiton"},
+        {"Gabriel": "Barboleto"},
+        {"Mclaren": "By Lando By Lando"}
+    ]
+
+    minha_collection_repository.insert_list_of_documents(list_of_documents)
